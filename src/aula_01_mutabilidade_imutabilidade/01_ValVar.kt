ed08dee781873911e38684944df7d77a86fc0c69
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2024-12-20 08:33:07 -0300

    :sparkles: feat: Immutability and mutability

---
+++ src/aula_01_mutabilidade_imutabilidade/01_ValVar.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // val == Value => We don't changed
+    val name = "Josileudo Rodrigues"
+
+    // var == Variable => We can change
+    // Age to 2024 year
+    var age = 25
+
+    // 2025
+    age = 26
+}
