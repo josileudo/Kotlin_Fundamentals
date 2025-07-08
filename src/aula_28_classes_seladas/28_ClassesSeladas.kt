8baae805c67c0056683240ebd053c3f4bf620851
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2025-07-08 11:59:49 -0300

    :sparkles: feat: Classes about sealed interfaces

--- src/aula_28_classes_seladas/28_ClassesSeladas.kt
+++ src/aula_28_classes_seladas/28_ClassesSeladas.kt
@@ -1,11 +1,24 @@
 package aula_28_classes_seladas
 
-import javax.xml.crypto.Data
-
 // Objetivo é controlar a herança dentro da aplicação
 
+sealed interface Animal {
+    data class Dog(val bread: String): Animal {
+        override fun sound(): String {
+            return "Au!"
+        }
+    }
+    data class Cat(val color: String): Animal {
+        override fun sound(): String {
+            return "Miau!"
+        }
+    }
+
+    fun sound(): String
+}
+
 sealed class Result {
-    data class Success(val data: String): Result()
+    data class Success(val data: String): Result() // Adiciona parentese para mostrar que esta sendo instanciado
     data class Error(val exception: Exception): Result()
     data object Loading: Result()
 }
@@ -16,6 +29,15 @@ fun handleResult(result: Result) {
             is Result.Error -> "Houve um erro! ${result.exception.message}"
             Result.Loading -> "Carregando..."
             is Result.Success -> "Sucesso: ${result.data}"
+        }
+    )
+}
+
+fun handleAnimal(animal: Animal) {
+    println(
+        when(animal) {
+            is Animal.Cat -> "O animal selecionado foi um gato de cor ${animal.color}. O som que ele faz é ${animal.sound()}"
+            is Animal.Dog -> "O animal selecionado foi um cachorro da raça ${animal.bread}. O som que ele faz é ${animal.sound()}"
         }
     )
 }
@@ -40,7 +62,11 @@ fun main() {
     val isContactInserted: Result = Database.insertContact(contact = "Fernando")
     val isContactUpdated: Result = Database.newContact(id = 1, newContact = "Luiz Marcelo")
 
+    val animalInserted: Animal = Animal.Cat(color = "Branco")
+
     handleResult(contacts)
     handleResult(isContactInserted)
     handleResult(isContactUpdated)
+
+    handleAnimal(animalInserted)
 }
